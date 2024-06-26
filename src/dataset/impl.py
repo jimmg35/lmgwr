6373347dc5596751d6a8879f77a96d4ba9ca1d54
jimmg35 <jim60308@gmail.com>
2024-06-26 14:06:31 +0800

    feat: init

---
+++ src/dataset/impl.py
@@ -0,0 +1,72 @@
+
+import numpy as np
+import pandas as pd
+from typing import List
+from .interface import IDataPoint, ISpatialDatasetParams, IDataset, IFieldInfo
+import logging
+
+
+class Dataset(IDataset):
+    dataPoints: List[IDataPoint] | None = None
+    fieldInfo: IFieldInfo | None = None
+
+    def __init__(self, param: ISpatialDatasetParams):
+        self.fieldInfo = param.fieldInfo
+        self._verify_fields(param.data)
+        self.dataPoints = self._create_data_points(param.data)
+
+    def _verify_fields(self, data: pd.DataFrame) -> None:
+        if self.fieldInfo is None:
+            raise ValueError("FieldInfo is not set")
+        required_fields = [
+            self.fieldInfo.response_field,
+            self.fieldInfo.coordinate_x_field,
+            self.fieldInfo.coordinate_y_field,
+            *self.fieldInfo.predictor_fields
+        ]
+        missing_fields = [
+            field for field in required_fields if field not in data.columns]
+        if missing_fields:
+            raise ValueError(
+                f"Missing fields in the dataset: {', '.join(missing_fields)}"
+            )
+        logging.info(f"$ Required fields are present in the dataset.")
+
+    def _create_data_points(self, data: pd.DataFrame) -> List[IDataPoint]:
+        if self.fieldInfo is None:
+            raise ValueError("FieldInfo is not set")
+        try:
+            data_points = []
+            for _, row in data.iterrows():
+                y = row[self.fieldInfo.response_field]
+                X = row[self.fieldInfo.predictor_fields].values
+                coordinate_x = row[self.fieldInfo.coordinate_x_field]
+                coordinate_y = row[self.fieldInfo.coordinate_y_field]
+                data_points.append(IDataPoint(y=y, X=np.array(
+                    X), coordinate_x=coordinate_x, coordinate_y=coordinate_y))
+            logging.info(f"$ Data points created successfully.")
+            return data_points
+        except Exception as e:
+            logging.error(f"Error creating data points: {e}")
+            raise e
+
+
+class SpatialDataset(Dataset):
+
+    def __init__(self, param: ISpatialDatasetParams):
+        super().__init__(param)
+
+
+if __name__ == '__main__':
+    synthetic_data = pd.read_csv(r'./data/synthetic_dataset.csv')
+
+    spatialDataset = SpatialDataset(
+        ISpatialDatasetParams(
+            fieldInfo=IFieldInfo(
+                predictor_fields=['temperature', 'moisture'],
+                response_field='pm25',
+                coordinate_x_field='coor_x',
+                coordinate_y_field='coor_y'
+            ), data=synthetic_data
+        )
+    )
