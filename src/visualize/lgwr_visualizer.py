69ab1cc601598538ddcfbbecf321243e0a60601c
jimmg35 <jim60308@gmail.com>
2025-04-03 05:08:11 +0800

    feat: update

--- src/visualize/lgwr_visualizer.py
+++ src/visualize/lgwr_visualizer.py
@@ -3,10 +3,13 @@ import json
 import numpy as np
 
 from src.dataset.spatial_dataset import SpatialDataset
+from src.visualize.lgwr_bandwidth_statistics import LgwrBandwidthStatistics
 import geopandas as gpd
-# from pysal.lib import weights
-# from pysal.explore import esda
 import matplotlib.pyplot as plt
+
+from libpysal.weights import Queen
+from splot.esda import plot_moran
+from esda.moran import Moran
 
 
 class LgwrVisualizer():
@@ -22,57 +25,65 @@ class LgwrVisualizer():
         self.dataset = dataset
         self.__load_log_json()
 
-    def summary_bandwidth_info(self) -> None:
-        optimized_bandwidths_info = self.model_info['bandwidth_optimization']
-        for info in optimized_bandwidths_info:
+    def descriptive_statistics(self, episode: int) -> LgwrBandwidthStatistics:
 
-            local_bandwidths = np.array(
-                json.loads(info['bandwidth'])
-            ).reshape(-1, 1)
-
-            max_bandwidth = np.max(local_bandwidths)
-            min_bandwidth = np.min(local_bandwidths)
-            median_bandwidth = np.median(local_bandwidths)
-            mean_bandwidth = np.mean(local_bandwidths)
-            variance_bandwidth = np.var(local_bandwidths)
-            mob_bandwidth = np.mean(np.abs(local_bandwidths - mean_bandwidth))
-
-            print(f"{'-'*40}")
-            print(f"Episode: {info['episode']} | Reward: {info['reward']}")
-            print(f"{'Bandwidth Statistics':^40}")
-            print(f"{'-'*40}")
-            print(f"{'Statistic':<20} | {'Value':<20}")
-            print(f"{'-'*40}")
-            print(f"{'Max':<20} | {max_bandwidth:.4f}")
-            print(f"{'Min':<20} | {min_bandwidth:.4f}")
-            print(f"{'Median':<20} | {median_bandwidth:.4f}")
-            print(f"{'Mean':<20} | {mean_bandwidth:.4f}")
-            print(f"{'Variance':<20} | {variance_bandwidth:.4f}")
-            print(f"{'Mean Absolute Deviation':<20} | {mob_bandwidth:.4f}")
-            print(f"{'-'*40}")
-
-    def plot_bandwidth_map_by_episode(self, episode: int) -> None:
         optimized_bandwidths_info = self.model_info['bandwidth_optimization']
         info = next(filter(lambda x: x['episode'] ==
                     episode, optimized_bandwidths_info), None)
         if info is None or self.dataset.geometry is None:
+            raise ValueError(
+                "Episode not found or geometry is None. Please check the episode number or geometry."
+            )
+
+        max_bandwidth, min_bandwidth, median_bandwidth, mean_bandwidth, variance_bandwidth, mob_bandwidth = self.basic_statistics(
+            episode
+        )
+
+        moran_i, moran_p = self.test_spatial_autocorrelation(episode)
+
+        result = LgwrBandwidthStatistics(
+            episode=info['episode'],
+            reward=info['reward'],
+            r2=info['r2'],
+            max_bandwidth=max_bandwidth,
+            min_bandwidth=min_bandwidth,
+            median_bandwidth=median_bandwidth,
+            mean_bandwidth=mean_bandwidth,
+            variance_bandwidth=variance_bandwidth,
+            mean_absolute_deviation=mob_bandwidth,
+            moran_i=moran_i,
+            moran_p_value=moran_p
+        )
+        return result
+
+    def basic_statistics(self, episode: int):
+        local_bandwidths = self._get_bandwidth_by_episode(episode)
+        max_bandwidth = np.max(local_bandwidths)
+        min_bandwidth = np.min(local_bandwidths)
+        median_bandwidth = float(np.median(local_bandwidths))
+        mean_bandwidth = float(np.mean(local_bandwidths))
+        variance_bandwidth = float(np.var(local_bandwidths))
+        mob_bandwidth = float(
+            np.mean(np.abs(local_bandwidths - mean_bandwidth)))
+        return max_bandwidth, min_bandwidth, median_bandwidth, mean_bandwidth, variance_bandwidth, mob_bandwidth
+
+    def test_spatial_autocorrelation(self, episode: int, permutations: int = 999) -> tuple[float, float]:
+        local_bandwidths = self._get_bandwidth_by_episode(episode)
+        w = Queen.from_dataframe(self.dataset.geometry)
+        w.transform = 'r'  # type: ignore
+        moran = Moran(local_bandwidths, w, permutations=permutations)
+        return moran.I, moran.p_sim
+
+    def plot_bandwidth_map_by_episode(self,
+                                      episode: int,
+                                      result: LgwrBandwidthStatistics,
+                                      save: bool = False,
+                                      ) -> None:
+        if self.dataset.geometry is None:
             return
+        local_bandwidths = self._get_bandwidth_by_episode(episode)
 
-        # Load the local bandwidth vector of the given episode
-        local_bandwidth_vector = np.array(
-            json.loads(info['bandwidth'])
-        ).reshape(-1, 1)
-
-        # Ensure the length of local_bandwidth_vector matches the length of the geometry
-        if len(local_bandwidth_vector) != len(self.dataset.geometry):
-            print(
-                "The length of local_bandwidth_vector does not match the length of the geometry.")
-            return
-
-        # Add the local_bandwidth_vector as a new column to the GeoDataFrame
-        self.dataset.geometry['local_bandwidth'] = local_bandwidth_vector
-
-        # Plot the GeoDataFrame with a gradient color based on local_bandwidth
+        self.dataset.geometry['local_bandwidth'] = local_bandwidths
         self.dataset.geometry.plot(
             column='local_bandwidth',
             cmap='viridis',
@@ -80,37 +91,44 @@ class LgwrVisualizer():
             legend_kwds={'label': "Local Bandwidth"}
         )
         plt.title(f"Bandwidth Map for Episode {episode}")
-        plt.show()
 
-    # def __calculate_moran_i(self):
-    #     if self.dataset.geometry is None:
-    #         return
+        textstr = '\n'.join((
+            f"Max: {result.max_bandwidth:.2f}",
+            f"Min: {result.min_bandwidth:.2f}",
+            f"Mean: {result.mean_bandwidth:.2f}",
+            f"Median: {result.median_bandwidth:.2f}",
+            f"Variance: {result.variance_bandwidth:.2f}",
+            f"MAD: {result.mean_absolute_deviation:.2f}",
+            f"AICc: {result.reward:.2f}",
+            f"R2: {result.r2:.2f}",
+            f"Moran's I: {result.moran_i:.3f}",
+            f"p-value: {result.moran_p_value:.3f}"
+        ))
 
-    #     # Ensure the 'local_bandwidth' column exists
-    #     if 'local_bandwidth' not in self.dataset.geometry.columns:
-    #         print("The 'local_bandwidth' column is missing in the dataset.")
-    #         return
+        plt.gcf().text(0.05, 0.95, textstr, fontsize=10,
+                       bbox=dict(facecolor='white', alpha=0.8),
+                       verticalalignment='top')
 
-    #     # Calculate Moran's I
+        if save:
+            map_path = os.path.join(self.log_path, 'bandwidth_map')
+            if not os.path.exists(map_path):
+                os.mkdir(map_path)
 
-    #     # Create spatial weights matrix
-    #     w = weights.Queen.from_dataframe(self.dataset.geometry)
-    #     w.transform = 'r'
-
-    #     # Extract the local_bandwidth column as a numpy array
-    #     local_bandwidth = self.dataset.geometry['local_bandwidth'].values
-
-    #     # Compute Moran's I
-    #     moran = esda.Moran(local_bandwidth, w)
-
-    #     print(f"Moran's I: {moran.I:.4f}")
-    #     print(f"Expected I: {moran.EI:.4f}")
-    #     print(f"p-value: {moran.p_sim:.4f}")
-    #     print(f"z-score: {moran.z_sim:.4f}")
+            plt.savefig(os.path.join(
+                map_path, f"bandwidth_map_episode_{episode}.png"
+            ))
+        else:
+            plt.show()
 
     def __load_log_json(self) -> None:
-
         with open(
             os.path.join(self.log_path, self.log_filename), 'r'
         ) as file:
             self.model_info = json.load(file)
+
+    def _get_bandwidth_by_episode(self, episode: int) -> np.ndarray:
+        info = next(filter(lambda x: x['episode'] == episode,
+                    self.model_info['bandwidth_optimization']), None)
+        if info is None:
+            raise ValueError("Episode not found.")
+        return np.array(json.loads(info['bandwidth'])).reshape(-1)
