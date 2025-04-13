283a95892199c4b302b9fc1eeefda8db7af17c0a
jimmg35 <jim60308@gmail.com>
2025-04-13 11:26:18 +0800

    feat: update

--- script-test-dgp.py
+++ script-test-dgp.py
@@ -1,6 +1,4 @@
-import statsmodels.api as sm
-from src.dgp.simulation import generate_processes, generate_data
-from src.dgp.plot import plot_s
+
 import numpy as np
 from src.model.gwr import GWR
 from src.dataset.simulated_spatial_dataset import SimulatedSpatialDataset
@@ -24,11 +22,6 @@ if __name__ == "__main__":
         sub_title=['b0', 'b1', 'b2'],
         size=field_size
     )
-    # dataset.plot(
-    #     b=np.vstack([X.T, y.T]),
-    #     sub_title=['intercept', 'X1', "X2", 'y'],
-    #     size=field_size
-    # )
 
     # Create a GWR kernel and GWR model.
     kernel = GwrKernel(dataset, 'bisquare')
@@ -45,20 +38,3 @@ if __name__ == "__main__":
         sub_title=['b0_gwr', 'b1_gwr', 'b2_gwr'],
         size=field_size
     )
-
-    # logger.save_model_info_json()
-
-
-# size = 40
-
-
-# # Prepare the design matrix with an intercept
-# X = np.hstack([np.ones((size * size, 1)),
-#                X1.reshape(-1, 1), X2.reshape(-1, 1)])
-
-# # Fit the OLS model
-# ols_model = sm.OLS(y, X).fit()
-
-# # Print the summary of the model
-# print(ols_model.summary())
-# print(ols_model.params)
