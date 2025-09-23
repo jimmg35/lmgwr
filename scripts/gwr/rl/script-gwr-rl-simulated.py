0f0b50523a852de4a43079fa17fd7fd15fbc00ec
jimmg35 <jimmg350@gmail.com>
2025-09-23 16:24:24 -0400

    Refactor: Improve and analyze GWR RL simulated demo

--- scripts/gwr/rl/script-gwr-rl-simulated.py
+++ scripts/gwr/rl/script-gwr-rl-simulated.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 from stable_baselines3 import PPO
 
 from src.optimizer.reinforce.gwr_optimizer import GwrOptimizerRL
@@ -8,10 +9,11 @@ from src.dataset.simulated_spatial_dataset import SimulatedSpatialDataset
 from src.kernel.gwr_kernel import GwrKernel
 from src.log.gwr_logger import GwrLogger
 from src.model.gwr import GWR
+from src.utility.distribution_test import normal_distribution_test
 
 # Hyperparameters for PPO training
-MAX_STEPS = 100
-TOTAL_TIMESTEPS = MAX_STEPS * 100
+MAX_STEPS = 10
+TOTAL_TIMESTEPS = MAX_STEPS * 10
 MIN_ACTION = -10
 MAX_ACTION = 10
 
@@ -27,18 +29,18 @@ if __name__ == '__main__':
     logger = GwrLogger()
 
     # Create a simulated dataset.
-    field_size = 10
-    spatialDataset = SimulatedSpatialDataset(field_size=field_size)
-    [b0, b1, b2] = spatialDataset.generate_processes()
-    [X, y] = spatialDataset.fit_y(b0, b1, b2)
+    field_size = 40
+    dataset = SimulatedSpatialDataset(field_size=field_size)
+    [b0, b1, b2] = dataset.generate_processes()
+    [X, y, err] = dataset.fit_y(b0, b1, b2)
 
     # Create a GWR kernel and GWR model.
     kernel = GwrKernel(
-        spatialDataset,
+        dataset,
         kernel_type='bisquare',
         kernel_bandwidth_type='adaptive'
     )
-    gwr = GWR(spatialDataset, kernel, logger)
+    gwr = GWR(dataset, kernel, logger)
 
     # Initial gwr gym environment
     env = GwrOptimizerRL(
@@ -48,7 +50,7 @@ if __name__ == '__main__':
         None,
         TOTAL_TIMESTEPS,
         min_bandwidth=MIN_BANDWIDTH,
-        max_bandwidth=spatialDataset.X.shape[0],
+        max_bandwidth=dataset.X.shape[0],
         min_action=MIN_ACTION,
         max_action=MAX_ACTION,
         max_steps=MAX_STEPS
@@ -65,3 +67,22 @@ if __name__ == '__main__':
         total_timesteps=TOTAL_TIMESTEPS
     )
     logger.append_info("PPO: PPO finished training.")
+
+    print(gwr.betas.shape)
+    [b0_gwr, b1_gwr, b2_gwr] = [gwr.betas[:, 0], gwr.betas[:, 1], gwr.betas[:, 2]]
+    # dataset.plot(
+    #     b=np.vstack([b0_gwr, b1_gwr, b2_gwr]),
+    #     sub_title=['b0_gwr', 'b1_gwr', 'b2_gwr'],
+    #     size=field_size
+    # )
+
+    pred_y = (b0_gwr * dataset.X[:, 0] + b1_gwr * dataset.X[:, 1] + b2_gwr *
+                dataset.X[:, 2] + dataset.err).reshape(-1, 1)
+    pred_err = pred_y - y
+    # dataset.plot(
+    #     b=np.vstack(pred_err.T),
+    #     sub_title=['prediction error'],
+    #     size=field_size
+    # )
+    normal_distribution_test(pred_err, title='Prediction Error', xlabel='Error')
+
