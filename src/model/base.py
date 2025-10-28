cf80fb3c1e609773ed4252d1dc15edbf900a27e6
jimmg35 <jimmg350@gmail.com>
2025-10-28 10:20:13 -0400

    l

--- src/model/base.py
+++ src/model/base.py
@@ -192,7 +192,18 @@ class Base:
         n = len(self.dataset)
         # RSS = np.sum(self.residuals ** 2)
         AIC = -2.0 * self.llf + 2.0 * (self.tr_S + 1)
-        AICc = -2.0 * self.llf + 2.0 * n * \
-            (self.tr_S + 1.0) / (n - self.tr_S - 2.0)
+        AICc = -2.0 * self.llf + 2.0 * n * (self.tr_S + 1.0) / (n - self.tr_S - 2.0)
         self.aic = AIC
         self.aicc = AICc
+        print("======================")
+        print(f"llf: {self.llf}")
+        print(f"tr_S: {self.tr_S}")
+        print("----------------------")
+        print(f"first: {-2.0 * self.llf}")
+        print(f"second: {2.0 * n * (self.tr_S + 1.0) / (n - self.tr_S - 2.0)}")
+        print("----------------------")
+        print(f"up: {2.0 * n * (self.tr_S + 1.0)}")
+        print(f"down: {n - self.tr_S - 2.0}")
+        print(f"AICc: {AICc} \n")
+
+
